cf2abad01d95d97f6fb32556922f7b0bfe6b5de4
shaneferrellwv <shaneferrellwv@gmail.com>
2022-08-01 15:14:12 -0400

    added custom tip feature and added more actions to calculate tip

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -13,20 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var customPercentageTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.billAmountTextField.becomeFirstResponder()
     }
 
     @IBAction func calculateTip(_ sender: Any) {
         
-        // Get bill amount form text filed input
+        // Get bill amount from text field input
         let bill = Double(billAmountTextField.text!) ?? 0
+        var customTipPercentage = Double(customPercentageTextField.text!) ?? 0
     
         
         // Get Total tip by mulitplying tip * tipPercentage
-        let tipPercentages = [0.15, 0.18, 0.2]
+        let tipPercentages = [0.15, 0.18, 0.2, customTipPercentage / 100]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
